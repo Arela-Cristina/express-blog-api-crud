2c8ab99066cba503fd52a1e9eb7a6498a3d22d0a
Arela-Cristina <instagrandine@gmail.com>
2024-11-20 16:07:41 +0100

    feat: config a basic server with express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Hello");
+//STEP 1
+const express = require("express"); //import
+const app = express(); // istanza = app
+const port = 1311; // #porta
+
+//STEP 3
+app.get("/", (req, res) => {
+  console.log("Server del mio nuovo blog");
+  res.send("Benvenuto al server del mio nuovo blog");
+});
+
+//STEP 2
+app.listen(port, () => {
+  console.log(`server in ascolto http://localhost:${port}`);
+});
