2bf8f6914c769cdd66db1ba5995c89a266ef480e
Arela-Cristina <instagrandine@gmail.com>
2024-11-20 21:00:46 +0100

    add new props for each object on database

--- data/data_base.js
+++ data/data_base.js
@@ -1,26 +1,31 @@
 const posts = [
     {
       title: "Post 1",
+      tipos: ['Dolce', 'Pane' ],
       img: "/images/ciambellone.jpeg",
       id: 1,
     },
     {
       title: "Post 2",
+      tipos:  ['Dolce', 'Cracker' ],
       img: "/images/cracker_barbabietola.jpeg",
       id: 2,
     },
     {
       title: "Post 3",
+      tipos:  ['Dolce', 'Pane', 'Salato' ],
       img: "/images/pane_fritto_dolce.jpeg",
       id: 3,
     },
     {
       title: "Post 4",
+      tipos:  ['Salato', 'Pasta' ],
       img: "/images/pasta_barbabietola.jpeg",
       id: 4,
     },
     {
       title: "Post 5",
+      tipos:  ['Dolce', 'Torta' ],
       img: "/images/torta_paesana.jpeg",
       id: 5,
     },
